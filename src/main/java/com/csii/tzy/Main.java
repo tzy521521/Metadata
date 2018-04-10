340081e80d71d6c84b0b75bf3df36c905059cf03
tzy521521 <843460950@qq.com>
2018-04-10 10:55:10 +0800

    2018.4.10-10:23

--- src/main/java/com/csii/tzy/Main.java
+++ src/main/java/com/csii/tzy/Main.java
@@ -4,10 +4,7 @@ import com.csii.tzy.database.*;
 import com.csii.tzy.utils.DBMetadataUtils;
 import com.csii.tzy.utils.FtUtil;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -18,78 +15,20 @@ public class Main {
                 "scott",
                 "scott"
         );
+        /**
+         * 创建完DBMetadataUtils之后，断开数据库连接。
+         * 仅仅初始化了dataSource、letterCase、dialect、introspector(DatabaseIntrospector)、catalogs和schemas。
+         * connection和databaseMetaData设置为了空？
+         * 这样做的目的？？？？
+         * 应该是如果没有清空databaseMetaData，在源代码里还继续使用databaseMetaData，那么万一再创建了这个databaseMetaData
+         * 之后，数据库的信息修改了，之前赋值的databaseMetaData就没有意义。
+         */
         DBMetadataUtils dbMetadataUtils =new DBMetadataUtils(dataSource);
         DatabaseConfig config = new DatabaseConfig("orcl","scott");
         List<IntrospectedTable> list = dbMetadataUtils.introspectTables(config);
 
-
-        //表集合
-        List<Map<String, Object>> tableList = new ArrayList<>();
-        for (IntrospectedTable table : list) {
-            Map<String, Object> tb = new HashMap<>();
-            System.out.println("表名称："+table.getName()+"  表注释："+table.getRemarks()+"  表类型："+table.getType());
-            if(table.getType()!=null){
-                tb.put("type",table.getType());
-            }
-            else
-                tb.put("type","没有？");
-            //表名称
-            tb.put("name", table.getName());
-            //表备注
-            if (table.getRemarks()!=null){
-                tb.put("remarks",table.getRemarks()+"。");
-            }else
-                tb.put("remarks"," ");
-            //表主键
-            String primaryKey="";
-            for (IntrospectedColumn column : table.getPrimaryKeyColumns()) {
-                primaryKey+=column.getName()+" ";
-            }
-            tb.put("primaryKey",primaryKey);
-            //表外键
-            String foreignKey="";
-            for (IntrospectedColumn column : table.getForeignKeyColumns()) {
-                primaryKey+=column.getName()+" ";
-            }
-            tb.put("foreignKey",foreignKey);
-            //表索引
-            String inFo="";
-            for (IndexInFo column : table.getInFoColumns()) {
-                inFo+=column.getColumnName()+" ";
-            }
-            tb.put("inFo",inFo);
-
-            //字段集合
-            List<Map<String, Object>> columnList = new ArrayList<>();
-            for (IntrospectedColumn column : table.getAllColumns()) {
-                System.out.println("字段注释"+column.getRemarks());
-                //字段
-                Map<String, Object> field = new HashMap<>();
-                field.put("name", column.getName());
-
-                if (column.getRemarks()!=null){
-                    field.put("remarks",column.getRemarks());
-                }else
-                    field.put("remarks","");
-                field.put("jdbcTypeName", column.getJdbcTypeName());
-                field.put("length",column.getLength());
-                if (column.isNullable()){
-                    field.put("nullable","Y");
-                }else {
-                    field.put("nullable","N");
-                }
-                if (column.getDefaultValue()!=null){
-                    field.put("default",""+column.getDefaultValue());
-                }else
-                    field.put("default","");
-                columnList.add(field);
-            }
-            tb.put("COLUMNS", columnList);
-            tableList.add(tb);
-        }
-        Map<String,Object> map =new HashMap<>();
-        map.put("table", tableList);
         FtUtil ftUtil = new FtUtil();
-        ftUtil.generateFile("/", "demo.xml", map, "D:/", "scott.doc");
+        ftUtil.generateFile("/", "demo.xml", list, "D:/", "scott.doc");
+
     }
 }
--- src/main/java/com/csii/tzy/utils/FtUtil.java
+++ src/main/java/com/csii/tzy/utils/FtUtil.java
@@ -1,66 +1,48 @@
 package com.csii.tzy.utils;
 
-        import com.csii.tzy.database.IndexInFo;
-        import com.csii.tzy.database.IntrospectedColumn;
-        import com.csii.tzy.database.IntrospectedTable;
-        import freemarker.template.Configuration;
-        import freemarker.template.Template;
-        import freemarker.template.TemplateException;
+import com.csii.tzy.database.IndexInFo;
+import com.csii.tzy.database.IntrospectedColumn;
+import com.csii.tzy.database.IntrospectedTable;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
-        import java.io.File;
-        import java.io.FileWriter;
-        import java.io.IOException;
-        import java.util.ArrayList;
-        import java.util.HashMap;
-        import java.util.List;
-        import java.util.Map;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FtUtil {
-    /**
-     * 获取模板
-     *
-     * @param templatesDir 例如"/templates"
-     * @return
-     */
-    public Template getTemplate(String templatesDir, String name) {
-        try {
-            //通过Freemaker的Configuration读取相应的ftl
-            Configuration cfg = new Configuration();
-            //设定去哪里读取相应的ftl模板文件
-            cfg.setClassForTemplateLoading(this.getClass(), templatesDir);
-            //在模板文件目录中找到名称为name的文件
-            Template temp = cfg.getTemplate(name);
-            return temp;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    /**
-     * Description: 根据模版生成文件 <br/>
 
-     */
-
+    /*
     public void generateFile(String templatesDir, String templateName, Map root, String outDir, String outFileName) {
         FileWriter out = null;
         try {
             //通过一个文件输出流，就可以写到相应的文件中
             out = new FileWriter(new File(outDir, outFileName));
-            Template temp = this.getTemplate(templatesDir, templateName);
+            Template temp = getTemplate(templatesDir, templateName);
             temp.process(root, out);
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (TemplateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (out != null) out.close();
+                if (out != null)
+                    out.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+     */
 
+    /**
+     * Description: 根据模版生成文件 <br/>
+     */
     public void generateFile(String templatesDir, String templateName, List<IntrospectedTable> list, String outDir, String outFileName) {
         Map root = getFtUtilMap(list);
         FileWriter out = null;
@@ -81,17 +63,43 @@ public class FtUtil {
             }
         }
     }
+    /**
+     * 获取模板
+     * @param templatesDir 例如"/templates"
+     * @return
+     */
+    private Template getTemplate(String templatesDir, String name) {
+        try {
+            //通过Freemaker的Configuration读取相应的ftl
+            Configuration cfg = new Configuration();
+            //设定去哪里读取相应的模板文件
+            cfg.setClassForTemplateLoading(this.getClass(), templatesDir);
+            //在模板文件目录中找到名称为name的文件
+            Template temp = cfg.getTemplate(name);
+            return temp;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     private Map<String,Object> getFtUtilMap(List<IntrospectedTable> list){
         //表集合
         List<Map<String, Object>> tableList = new ArrayList<>();
         for (IntrospectedTable table : list) {
-            //表
             Map<String, Object> tb = new HashMap<>();
-            System.out.println(table.getName());
+            System.out.println("表名称："+table.getName()+"  表注释："+table.getRemarks()+"  表类型："+table.getType());
+            if(table.getType()!=null){
+                tb.put("type",table.getType());
+            }
+            else
+                tb.put("type","没有？");
             //表名称
             tb.put("name", table.getName());
             //表备注
-            tb.put("remarks",table.getRemarks());
+            if (table.getRemarks()!=null){
+                tb.put("remarks",table.getRemarks()+"。");
+            }else
+                tb.put("remarks"," ");
             //表主键
             String primaryKey="";
             for (IntrospectedColumn column : table.getPrimaryKeyColumns()) {
@@ -111,14 +119,17 @@ public class FtUtil {
             }
             tb.put("inFo",inFo);
 
-
             //字段集合
             List<Map<String, Object>> columnList = new ArrayList<>();
             for (IntrospectedColumn column : table.getAllColumns()) {
                 //字段
                 Map<String, Object> field = new HashMap<>();
                 field.put("name", column.getName());
-                field.put("remarks",column.getRemarks());
+
+                if (column.getRemarks()!=null){
+                    field.put("remarks",column.getRemarks());
+                }else
+                    field.put("remarks","");
                 field.put("jdbcTypeName", column.getJdbcTypeName());
                 field.put("length",column.getLength());
                 if (column.isNullable()){
@@ -126,13 +137,12 @@ public class FtUtil {
                 }else {
                     field.put("nullable","N");
                 }
-                if (column.getDefaultValue()==null){
-                    field.put("default","");
-                }else
+                if (column.getDefaultValue()!=null){
                     field.put("default",""+column.getDefaultValue());
+                }else
+                    field.put("default","");
                 columnList.add(field);
             }
-
             tb.put("COLUMNS", columnList);
             tableList.add(tb);
         }
