045118e4887df087d08d819e866a05b37da9b642
tzy521521 <843460950@qq.com>
2018-04-13 11:47:26 +0800

    2018.4.13-11:31

--- src/main/java/com/csii/tzy/Main.java
+++ src/main/java/com/csii/tzy/Main.java
@@ -9,14 +9,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception{
 
+        //jdbc:oracle:thin:@//115.182.90.203:15217/xe jdbc:oracle:thin:@//localhost:1521/orcl
+        //user:eip_study   pwd:eip_study scott scott
          SimpleDataSource dataSource=new SimpleDataSource(
                 Dialect.ORACLE,
-                "jdbc:oracle:thin:@//115.182.90.203:15217/xe",
-                "eip_study",
-                "eip_study"
+                "jdbc:oracle:thin:@//localhost:1521/orcl",
+                "scott",
+                "scott"
         );
         DBMetadataUtils dbMetadataUtils =new DBMetadataUtils(dataSource);
-        DatabaseConfig config = new DatabaseConfig("xe","EIP_STUDY");
+        DatabaseConfig config = new DatabaseConfig("orcl","scott");
         List<IntrospectedTable> list = dbMetadataUtils.introspectTables(config);
 
         FtUtil ftUtil = new FtUtil();
