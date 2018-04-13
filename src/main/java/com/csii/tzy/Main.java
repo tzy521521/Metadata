4d70d1e78990c9e70c447c44378100fad0a78a9c
tzy521521 <843460950@qq.com>
2018-04-13 11:46:19 +0800

    2018.4.13-11:31

--- src/main/java/com/csii/tzy/Main.java
+++ src/main/java/com/csii/tzy/Main.java
@@ -9,8 +9,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        //jdbc:oracle:thin:@//115.182.90.203:15217/xe jdbc:oracle:thin:@//localhost:1521/orcl
-        //user:eip_study   pwd:eip_study scott scott
          SimpleDataSource dataSource=new SimpleDataSource(
                 Dialect.ORACLE,
                 "jdbc:oracle:thin:@//115.182.90.203:15217/xe",
